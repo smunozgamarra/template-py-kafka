acb8b0d747c50836723e57bb6b2a50f86004ede8
Sergio Muñoz <sergio.munoz@sdrggroup.com>
2023-07-18 20:59:07 +0200

    first basic version

--- entrypoint.py
+++ entrypoint.py
@@ -1,12 +1,37 @@
-from pyspark.python.pyspark.shell import spark
 from pyspark.sql import SparkSession
+import yaml
+from yaml.loader import SafeLoader
+from src.transform.transform import Transfom
+from src.utils import config_logger
+import logging
 
 if __name__ == '__main__':
+
+    logger = config_logger()
+    logger.setLevel(logging.INFO)
+
+    with open('config.yml') as f:
+        data = yaml.load(f, Loader=SafeLoader)
+
+    logger.debug("Starting %s process", data['pipeline_id'])
+    spark = SparkSession \
+        .builder \
+        .appName(data["pipeline_id"]) \
+        .getOrCreate()
     # Subscribe to 1 topic
+
     df = spark \
         .readStream \
         .format("kafka") \
-        .option("kafka.bootstrap.servers", "host1:port1,host2:port2") \
-        .option("subscribe", "topic1") \
-        .load()
-    df.selectExpr("CAST(key AS STRING)", "CAST(value AS STRING)")
+        .option("kafka.bootstrap.servers", data["source"]["hosts"]) \
+        .option("subscribe", data["source"]["topic"]) \
+        .load() \
+        .selectExpr("CAST(key AS STRING)", "CAST(value AS STRING)")
+
+    df_transformed = Transfom.run(logger=logger, df=df)
+    df_transformed.writeStream \
+        .format("kafka") \
+        .option("checkpointLocation", data["destination"]["checkpoint"]) \
+        .option("kafka.bootstrap.servers", data["destination"]["hosts"]) \
+        .option("topic", data["destination"]["topic"]) \
+        .trigger(continuous="1 second").start().awaitTermination()
---
+++ src/transform/transform.py
@@ -0,0 +1,10 @@
+class Transfom:
+    def __init__(self):
+        None
+
+    def run(logger, df):
+        logger.info("Starting transformation...")
+        '''
+            TODO: Add here your transformation code
+        '''
+        return df
